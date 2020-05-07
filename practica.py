2e86e7bad56cfca3b3d3018b79456b672b7cd3b1
Steven <steven96_layna@hotmail.com>
2020-05-07 13:30:46 -0500

    Tercer Captura

--- practica.py
+++ practica.py
@@ -5,3 +5,11 @@ print("mi nombre es: ")
 print("Steven")
 print("Y mi Apellido es:" )
 print("Layna")
+print("Ahora vamos a hacer un if")
+print("listos.....")
+
+num = 12
+numd = 14
+
+if num < numd:
+    print("La variable numd es mayor a la num")
