bbaa1eb4d20d662756f2deeefd3f1aebf2d1d0f7
Steven <steven96_layna@hotmail.com>
2020-05-07 14:05:13 -0500

    Cuarto commit de la Practica 2

--- practica2.py
+++ practica2.py
@@ -1,2 +1,3 @@
 print("Soy el archivo de la practica 2 de git - Soy el Segundo Commit")
 print("Hola soy el segundo el commi de la practica 2")
+print("Hola soy el tercer y ultimo restore de la practicca 2 para git")
